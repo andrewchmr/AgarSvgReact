6ca3f331e9f05527c2b913a9f2d0937fb41e2c71
Andrzej Chemerinskiy <andrzej.chemerinskiy@blue-veery.com>
2019-10-06 15:03:10 +0200

    Updates

---
+++ precache-manifest.afedc7154ec20789733be3463c2e22cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438456dbcd0a305c5fea7051e9bf9c77",
+    "url": "/AgarSvgReact/index.html"
+  },
+  {
+    "revision": "085e67b27c3e14f877ef",
+    "url": "/AgarSvgReact/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "45e721576e57b85d427c",
+    "url": "/AgarSvgReact/static/js/2.e75db994.chunk.js"
+  },
+  {
+    "revision": "085e67b27c3e14f877ef",
+    "url": "/AgarSvgReact/static/js/main.aba9140b.chunk.js"
+  },
+  {
+    "revision": "9797a9e47595a6ed4cba",
+    "url": "/AgarSvgReact/static/js/runtime-main.eaf5115a.js"
+  }
+]);
